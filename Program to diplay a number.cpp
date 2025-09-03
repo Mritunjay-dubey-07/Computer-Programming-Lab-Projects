848891ee07f87f34432fd323413002bfb9eee7bf
Mritunjay-dubey-07 <25cb1mr81@mitsgwl.ac.in>
2025-09-03 17:21:12 +0530

    Create Program to diplay a number.cpp

---
+++ Program to diplay a number.cpp
@@ -0,0 +1,10 @@
+// Program to Display my profile 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My name is Mritunjay Dubey" << endl;
+    cout << "I am enrolled in CSBS course in MITS-DU" << endl;
+    cout << "I have been born and braught up in Gwalior";
+    return 0;
+}

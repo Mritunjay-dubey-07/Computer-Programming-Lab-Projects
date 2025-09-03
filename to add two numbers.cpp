65015250b35e196187c519eda6e46f53754dd1ba
Mritunjay-dubey-07 <25cb1mr81@mitsgwl.ac.in>
2025-09-03 17:21:53 +0530

    Create to add two numbers.cpp

---
+++ to add two numbers.cpp
@@ -0,0 +1,20 @@
+// Adding two numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num_1;
+    int num_2;
+    
+    cout << "Enter a number: ";
+    cin >> num_1;
+    
+    cout << "Enter another number: ";
+    cin >> num_2;
+    
+
+    cout << "The addition of both the numbers is: " << num_1 + num_2;
+    
+    return 0;
+}
